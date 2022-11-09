0cca1c98ba7d2ed387fad2f90f503a3f90efa44f
sajixavier <sajixavier007@yahoo.com>
2022-11-09 21:16:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,20 +8,22 @@ import (
 
 func main() {
 
+	// list of websites to check
 	links := []string{
 		"https://www.google.com/",
 		"https://www.facebook.com/",
 		"https://www.twitter.com/",
 	}
 
+	// creating a channel
 	c := make(chan string)
 
+	// looping through links 
 	for _, link := range links {
 		go checkStatus(link, c)
 	}
 
-	// printOneTime(links, c)
-
+	// Run status checker
 	checkForEver(c)
 }
 
@@ -30,6 +32,7 @@ func main() {
  */
 func checkOneTime(links []string, c chan string) {
 	for i := 0; i < len(links); i++ {
+		// wait for the message from goroutine
 		<-c
 	}
 }
@@ -39,6 +42,7 @@ func checkOneTime(links []string, c chan string) {
  */
 func checkForEver(c chan string) {
 	for l := range c {
+		// start goroutine
 		go func(link string) {
 			time.Sleep(5 * time.Second)
 			checkStatus(link, c)
@@ -50,11 +54,13 @@ func checkStatus(link string, c chan string) {
 	resp, err := http.Head(link)
 	if err != nil || resp.StatusCode != 200 {
 		fmt.Printf("%s seems to be down\n", link)
+		// passing the message to channel
 		c <- link
 		return
 	}
 
 	fmt.Printf("%s is up!\n", link)
+	// passing the message to channel
 	c <- link
 
 }
